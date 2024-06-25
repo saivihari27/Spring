3d7fa5019e6b835242652ce7f67a36d27a47f8a4
saivihari27 <158553103+saivihari27@users.noreply.github.com>
2024-06-25 14:59:15 +0530

    Add files via upload

---
+++ BootRestToGenerateExcelSheet/src/main/java/com/springboot/BootRestExampleWithDbApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestExampleWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestExampleWithDbApplication.class, args);
+	}
+
+}
